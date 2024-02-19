51776404df689190f6a2ca2991ee11445e4e2fbb
Flemming <flemming.wu@gmail.com>
2024-02-19 15:22:31 -0800

    remove vars from constants.py

--- dags/config/constants.py
+++ dags/config/constants.py
@@ -1,2 +1,5 @@
-RAW_DATA_PATH = 'data/raw/'
-STAGING_DATA_PATH = 'data/staging'
+from pathlib import Path
+
+DATA_PATH = Path('data/')
+RAW_DATA_PATH = Path('data/raw/')
+STAGING_DATA_PATH = Path('data/staging/')
