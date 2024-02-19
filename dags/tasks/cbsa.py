7acfc4ba82ebf0fdc108d36c2a49466dfd144bf3
Flemming <flemming.wu@gmail.com>
2024-02-19 15:21:30 -0800

    add correct variable import in cbsa file

--- dags/tasks/cbsa.py
+++ dags/tasks/cbsa.py
@@ -1,30 +1,29 @@
 import requests
-import os
-
 import polars as pl
 
-from config.constants import RAW_DATA_PATH
+from config.constants import DATA_PATH
 
+from airflow.models import Variable
 
-def get_cbsa_codes() -> pl.DataFrame:
+EMAIL = Variable.get('EMAIL')
+API_KEY = Variable.get('API_KEY')
+
+def get_cbsa_codes(ti, **kwargs):
     '''Grabs CBSA lists from AQS API endpoint and returns Polars DataFrame'''
-
-    API_KEY = os.getenv('API_KEY')
-    EMAIL = os.getenv('EMAIL')
-
     url = f'https://aqs.epa.gov/data/api/list/cbsas?email={EMAIL}&key={API_KEY}'
 
     response = requests.get(url)
 
-    if response.status_code != 200:
-        print(f'An error has occurred: status code: {response.status_code}')
-        return 
+    if response.status_code == 200:
+        data = response.json()['Data']
+        ti.xcom_push(key='api_data', value=data)
+    else:
+        raise ValueError(f'API request failed with status code {response.status_code}.')
 
-    data = response.json()['Data']
+def write_cbsa_parquet_data(ti, **kwargs):
+    '''Accepts cbsa codes in json data and writes out parquet file to data lake'''
+    data = ti.xcom_pull(task_ids='query_api', key='api_data')
     df = pl.DataFrame(data)
-
-    return df
-
-def write_cbsa_parquet_data(df: pl.DataFrame) -> None:
-    '''Accepts cbsa codes in Polars DataFrame and writes out parquet file to data lake'''
-    df.write_parquet(f'{RAW_DATA_PATH}/cbsa.parquet')
+    path = DATA_PATH / 'cbsa.parquet'
+    with path.open('wb') as file:
+        df.write_parquet(file)
