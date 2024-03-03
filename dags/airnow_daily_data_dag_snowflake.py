244b8e474f1f11b83dd69a830e01969c0ab7af85
Flemming Wu <flemming.wu@gmail.com>
2024-03-03 14:30:55 -0800

    add snowflake dag and callables files

---
+++ dags/airnow_daily_data_dag_snowflake.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from airflow.utils.task_group import TaskGroup
+
+from plugins.callables.airnow import *
+from plugins.callables.sql import create_snowflake_tables_if_not_existing
+
+from cosmos import DbtTaskGroup, ExecutionConfig, ProfileConfig, ProjectConfig, RenderConfig
+
+
+
+
+
+# cosmos setup
+profile_config = ProfileConfig(
+    profile_name='airnow_aqs', 
+    target_name='prod',
+    profiles_yml_filepath='/home/airflow/.dbt/profiles.yml'
+)
+
+
+with DAG(
+    'airnow_daily_data_snowflake',
+    default_args={
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=1),
+        # 'queue': 'bash_queue',
+        # 'pool': 'backfill',
+        # 'priority_weight': 10,
+        # 'wait_for_downstream': False,
+        # 'sla': timedelta(hours=2),
+        # 'execution_timeout': timedelta(seconds=300),
+        # 'on_failure_callback': some_function, # or list of functions
+        # 'on_success_callback': some_other_function, # or list of functions
+        # 'on_retry_callback': another_function, # or list of functions
+        # 'sla_miss_callback': yet_another_function, # or list of functions
+        # 'trigger_rule': 'all_success'
+    },
+    description='Extracts hourly-updated air quality measurements from Airnow API every day',
+    start_date=days_ago(4), # data is updated continuously for 48 hours after posting
+    # start_date=datetime(2024, 1, 1),  # backfill starting from Jan 1, 2024
+    end_date=days_ago(4, hour=23),
+    schedule_interval='0 0 * * *', # daily at midnight
+    tags=['airnow'],
+    max_active_runs=1, # prevent OOM issues when Docker container hosted on local
+    catchup=True
+) as dag:
+    
+    create_staging_tables_if_not_existing_task = PythonOperator(
+        task_id='create_staging_tables_if_not_existing_task',
+        python_callable=create_snowflake_tables_if_not_existing,
+        provide_context=True
+    )
+
+
+
+    done = EmptyOperator(task_id='done')
+
+
+    create_staging_tables_if_not_existing_task >> done
